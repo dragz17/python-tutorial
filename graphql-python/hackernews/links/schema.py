f8fcd7881a5fb052e26de64dd9faeb31d31485c3
lucifer <psinghal4210@gmail.com>
2018-08-27 02:06:25 +0530

    Create mutation class, arguments, mutation method and mutations that our app can resolve

--- graphql-python/hackernews/links/schema.py
+++ graphql-python/hackernews/links/schema.py
@@ -12,3 +12,28 @@ class Query(graphene.ObjectType):
 
     def resolve_links(self, info, **kwargs):
         return Link.objects.all()
+
+#Defines a mutation class, data server can send to client after mutation
+class CreateLink(graphene.Mutation):
+    id = graphene.Int()
+    url = graphene.String()
+    description = graphene.String()
+
+    #2Defines the data you can send to the server
+    class Arguments:
+        url = graphene.String()
+        description = graphene.String()
+
+    #3The mutation method
+    def mutate(self, info, url, description):
+        link = Link(url=url, description=description)
+        link.save()
+
+        return CreateLink(
+            id=link.id,
+            url=link.url,
+            description=link.description,
+        )
+#Creates a mutation class with a field to be resolved
+class Mutation(graphene.ObjectType):
+    create_link = CreateLink.Field()
